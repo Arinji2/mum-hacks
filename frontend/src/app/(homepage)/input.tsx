821458022a7c74f5c1a4c237f7fa508d444aec6d
Arinji2 <arinjaydhar205@gmail.com>
2025-11-29 04:47:13 +0530

    misc: frontend: Fix dummy to input

--- frontend/src/app/(homepage)/input.tsx
+++ frontend/src/app/(homepage)/input.tsx
@@ -1,14 +1,18 @@
-import { Plus, Search } from "lucide-react";
+import { Search } from "lucide-react";
 
 export function Input() {
 	return (
 		<div className="w-full flex flex-row items-stretch justify-start gap-3">
 			<div className="w-full p-3  border-[3px] border-black  h-fit flex flex-row items-center justify-start gap-2">
 				<Search className="text-slate-600 text-lg" />
-				<p className="text-slate-600 text-lg">Search Products...</p>
+				<input
+					type="text"
+					className=" w-full  outline-none text-black text-lg"
+					placeholder="Search Products..."
+				/>
 			</div>
 			<div className="h-auto aspect-square shrink-0 flex bg-[#FFFF00] items-center justify-center  border-[3px] border-black ">
-				<Plus className="text-black text-5xl" strokeWidth={4} />
+				<Search className="text-black text-5xl" strokeWidth={4} />
 			</div>
 		</div>
 	);
